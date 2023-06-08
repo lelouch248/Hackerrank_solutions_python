0e28b3239afdc9af2c87150a729342812f3a56e6
Karthik G <68554363+lelouch248@users.noreply.github.com>
2023-06-08 10:03:03 +0530

    Update Apple_and_oranges.py

--- Algorithm_Implementation/Apple_and_oranges.py
+++ Algorithm_Implementation/Apple_and_oranges.py
@@ -10,3 +10,14 @@ def countApplesAndOranges(s, t, a, b, apples, oranges):
     for i in range(oranges_length):
         if b+oranges[i]>=s and b+oranges[i]<=t:
             o_count+=1
+    
+    print(f"{a_count}\n{o_count}")
+    
+    
+    # optimized solution 
+    
+    def countApplesAndOranges(s, t, a, b, apples, oranges):
+    a_count = sum(s <= (a + apple) <= t for apple in apples)
+    o_count = sum(s <= (b + orange) <= t for orange in oranges)
+    
+    print(f"{a_count}\n{o_count}")
